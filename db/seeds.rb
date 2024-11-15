fdfc1286f6aa71817b2c354dd14fdfcc47995429
JeanRemi Charrier <jeanremi.char@gmail.com>
2024-11-15 15:39:34 +0100

    chill-flix push

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,16 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-require "json"
 require "open-uri"
-
-url = JSON.parse(URI.open("https://tmdb.lewagon.com/movie/top_rated").read)
-
-url["results"].first(50).each_with_index do |movie_data, index|
+require "json"
+url = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=3&api_key=b3e4ed8ed2bcc7f2900cf7ed578deba8"
+movie_list = URI.open(url).read
+movies = JSON.parse(movie_list)["results"]
+puts "Creating movies"
+movies.each do |movie|
   Movie.create(
-    title: movie_data["original_title"],
-    overview: movie_data["overview"],
-    poster_url: "https://image.tmdb.org/t/p/w500#{movie_data["poster_path"]}",
-    rating: movie_data["vote_average"]
-  )
+    title: movie["original_title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie["backdrop_path"]}",
+    rating: movie["vote_average"].round(1))
 end
+puts "#{Movie.count} movies created!"
